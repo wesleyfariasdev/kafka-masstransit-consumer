8eb819280ecd39228e9fbb9feac0ebb87bfee863
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-09-28 17:35:39 -0300

    feat: adiciona entidade de Livros

---
+++ src/Domain/Entites/Livro.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Enums;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Domain.Entites;
+
+public sealed class Livro
+{
+    public Livro(string titulo, string autor, string iSBN, int anoPublicacao, Status status)
+    {
+        Id = ObjectId.GenerateNewId().ToString();
+        Titulo = titulo;
+        Autor = autor;
+        ISBN = iSBN;
+        AnoPublicacao = anoPublicacao;
+        Status = status;
+    }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    [BsonElement("titulo_livro")]
+    public string Titulo { get; set; }
+    [BsonElement("autor")]
+    public string Autor { get; set; }
+    [BsonElement("isbn")]
+    public string ISBN { get; set; }
+    [BsonElement("ano_publicacao")]
+    public int AnoPublicacao { get; set; }
+    [BsonElement("status")]
+    public Status Status { get; set; }
+
+    public void MarcarComoLido() =>
+           Status = Status.Lido;
+
+    public void MarcarComoLendo() =>
+           Status = Status.Lendo;
+
+    public void MarcarComoQueroLer() =>
+           Status = Status.QueroLer;
+}
