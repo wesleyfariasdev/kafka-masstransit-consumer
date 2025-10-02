8adeefe02f8919b3bebeb698b1182af095f2eb91
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-10-02 19:38:31 -0300

    feat: adiciona contexto para Livros no mongoDb

---
+++ src/Infraestructure/Data/ILivroContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+using MongoDB.Driver;
+
+namespace Infraestructure.Data;
+
+internal interface ILivroContext
+{
+    IMongoCollection<Livro> Livros { get; }
+}
---
+++ src/Infraestructure/Data/LivroContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using MongoDB.Driver;
+
+namespace Infraestructure.Data;
+
+public class LivroContext : ILivroContext
+{
+    private readonly IMongoDatabase _database;
+    public IMongoCollection<Livro> Livros { get; }
+
+    public LivroContext(string mongoConnectionString,
+                        string mongoDatabaseName,
+                        string mongoCollectionName)
+    {
+        MongoConnectionString = mongoConnectionString;
+        MongoDatabaseName = mongoDatabaseName;
+        MongoCollectionName = mongoCollectionName;
+
+        var client = new MongoClient(MongoConnectionString);
+        _database = client.GetDatabase(MongoDatabaseName);
+        Livros = _database.GetCollection<Livro>(MongoCollectionName);
+    }
+
+    public string MongoConnectionString { get; }
+    public string MongoDatabaseName { get; }
+    public string MongoCollectionName { get; }
+}
