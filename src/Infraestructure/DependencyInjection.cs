f5aaa1af93eb870d109a90de7f3bfe3e8b412622
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-10-02 19:40:52 -0300

    feat: adiciona dependency injection

--- src/Infraestructure/DependencyInjection.cs
+++ src/Infraestructure/DependencyInjection.cs
@@ -1,5 +1,31 @@
-﻿namespace Infraestructure;
+﻿using Infraestructure.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-internal class DependencyInjection
+namespace Infraestructure;
+
+public static class DependencyInjection
 {
+    public static void RegisterServices(IServiceCollection services)
+    {
+        services.AddSingleton<ILivroContext>(sp =>
+        {
+            var configuration = sp.GetRequiredService<IConfiguration>();
+            var mongoSettings = configuration.GetSection("MongoDbSettings");
+
+            if (!mongoSettings.Exists() ||
+                string.IsNullOrWhiteSpace(mongoSettings["ConnectionString"]) ||
+                string.IsNullOrWhiteSpace(mongoSettings["DatabaseName"]) ||
+                string.IsNullOrWhiteSpace(mongoSettings["CollectionName"]))
+            {
+                throw new InvalidOperationException("As configurações do MongoDB (ConnectionString, DatabaseName, CollectionName) devem ser fornecidas em 'MongoDbSettings'.");
+            }
+
+            return new LivroContext(
+                mongoSettings["ConnectionString"],
+                mongoSettings["DatabaseName"],
+                mongoSettings["CollectionName"]
+            );
+        });
+    }
 }
