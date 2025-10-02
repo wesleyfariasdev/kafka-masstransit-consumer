32d73161ac7f5f4f9a34b4e26d26bf386c1170d9
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-10-02 19:41:35 -0300

    feat: adiciona teste para validar o context

---
+++ tests/UnitTest/Infraestructure/Data/LivroContextTest.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Infraestructure.Data;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace UnitTest.Infrastructure.Data;
+
+public class LivroContextTests
+{
+    [Fact(DisplayName = "Adiciona livro no MongoDB via contexto")]
+    [Trait("Infrastructure", "LivroContext")]
+    public async Task DeveAdicionarLivroNoMongoDB()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { "MongoDbSettings:ConnectionString", "mongodb://localhost:27017" },
+                { "MongoDbSettings:DatabaseName", "ConsumerServicesDb" },
+                { "MongoDbSettings:CollectionName", "Livros" }
+            })
+            .Build();
+        var context = new LivroContext(
+            configuration["MongoDbSettings:ConnectionString"],
+            configuration["MongoDbSettings:DatabaseName"],
+            configuration["MongoDbSettings:CollectionName"]
+        );
+        var livro = new Livro
+        (
+            "1984",
+            "George Orwell",
+            "978-0-452-28423-4",
+            1949,
+            Status.Lendo
+        );
+
+        // Act
+        await context.Livros.InsertOneAsync(livro);
+        var livroSalvo = await context.Livros.Find(l => l.Id == livro.Id).FirstOrDefaultAsync();
+
+        // Assert
+        Assert.NotNull(livroSalvo);
+        Assert.Equal(livro.Titulo, livroSalvo.Titulo);
+        Assert.Equal(livro.Id, livroSalvo.Id);
+    }
+}
