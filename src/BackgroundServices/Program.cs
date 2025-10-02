f5cfdf78de1743327980c30391898c3f4a558a26
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-10-02 19:41:53 -0300

    registra injeção de dependencia

--- src/BackgroundServices/Program.cs
+++ src/BackgroundServices/Program.cs
@@ -1,7 +1,10 @@
 using BackgrondServices;
+using Infraestructure;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+DependencyInjection.RegisterServices(builder.Services);
 
 var host = builder.Build();
 host.Run();
